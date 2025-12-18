97059068815db2e356325ef630795dd9e21ecb40
Wilhelm Simus <wilhelm.simus@gmail.com>
2025-12-19 00:39:18 +0100

    Build template file

---
+++ dayXX.py
@@ -0,0 +1,15 @@
+file1 = open("input/testXX.txt")
+file2 = open("input/inputXX.txt")
+
+def parseInput(inputFile):
+    inputList = []
+    for line in inputFile:
+        if(line[0] == '\n'): # keep empty lines as empty strings
+            inputList.append("")
+        elif(line[-1] == '\n'): # remove line break from line
+            inputList.append(line[:-1])
+        else:
+            inputList.append(line) # last line has no line break
+
+input1 = parseInput(file1)
+input2 = parseInput(file2)
