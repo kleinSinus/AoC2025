8c9ea6de2a9adbb769fda2004dfcadbd34ac42a2
Wilhelm Simus <wilhelm.simus@gmail.com>
2025-12-19 06:05:41 +0100

    Day 7 - Init

---
+++ day07.py
@@ -0,0 +1,16 @@
+file1 = open("input/test07.txt")
+file2 = open("input/inputX07.txt")
+
+def parseInput(inputFile):
+    inputList = []
+    for line in inputFile:
+        if(line[0] == '\n'): # keep empty lines as empty strings
+            inputList.append("")
+        elif(line[-1] == '\n'): # remove line break from line
+            inputList.append(line[:-1])
+        else:
+            inputList.append(line) # last line has no line break
+    return inputList
+
+input1 = parseInput(file1)
+input2 = parseInput(file2)
